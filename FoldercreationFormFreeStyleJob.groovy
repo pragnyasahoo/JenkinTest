ace2a505249f25dce8c389c353d323b5c7bd2f2d
pragnyasahoo <pragnya.pradip@gmail.com>
2019-09-14 12:24:21 +0530

    freestyle job for creation folder

--- FoldercreationFormFreeStyleJob.groovy
+++ FoldercreationFormFreeStyleJob.groovy
@@ -1,26 +1,7 @@
-job('example-1') {
+job('Create Folder freestyle') {
     steps {
         dsl {
-            external('projectA.groovy', 'projectB.groovy')
-            external('projectC.groovy')
-            removeAction('DISABLE')
-            ignoreExisting()
-            additionalClasspath('lib')
-        }
-    }
-}
-
-job('example-2') {
-    steps {
-        dsl(['projectA.groovy', 'projectB.groovy'], 'DELETE')
-    }
-}
-
-job('example-3') {
-    steps {
-        dsl {
-            text(readFileFromWorkspace('more-jobs.groovy'))
-            removeAction('DELETE')
+            external('FoldercreationScript.groovy')          
         }
     }
 }
