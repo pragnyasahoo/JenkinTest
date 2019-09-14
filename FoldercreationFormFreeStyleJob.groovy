ecd89cf33f8e11d59b814b68aaa94b4027e95bb0
pragnyasahoo <pragnya.pradip@gmail.com>
2019-09-14 12:51:58 +0530

    freestyle job for creation folder

--- FoldercreationFormFreeStyleJob.groovy
+++ FoldercreationFormFreeStyleJob.groovy
@@ -1,4 +1,8 @@
 freeStyleJob('CreateFolderfreestyle') {
+	scm
+	{
+		github('pragnyasahoo/JenkinTest','master','ssh')
+	}
     steps {
         dsl {
             external('FoldercreationScript.groovy')          
