9d32e9dabfc3d0a742552b9f803ae849e4119810
pragnyasahoo <pragnya.pradip@gmail.com>
2019-09-21 12:10:21 +0530

    pipeline job

--- pipelinejob.groovy
+++ pipelinejob.groovy
@@ -1,14 +1,6 @@
 pipelineJob('pipelinejob') {
 
-     scm {
-        git {
-            remote {
-                name('origin')
-                url('https://github.com/pragnyasahoo/JenkinTest.git')
-            }
-        }
-      }
-
+    
     definition {
         cps {
             //script(readFileFromWorkspace('PipeLinesript.groovy'))
